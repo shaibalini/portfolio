ac20d452011acb37a8fb1a0b02237350e7dde8bf
Shaibalini Sahoo <shaibalini@gmail.com>
2019-04-15 10:46:54 +0530

    good one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TechnicalComponent } from './technical/technical.component';
+import { ExtraCurricularComponent } from './extra-curricular/extra-curricular.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'extra', component: ExtraCurricularComponent },
+  { path: 'tech', component: TechnicalComponent },
+  { path: 'contact', component: ContactComponent},
+  { path: '', component: HomePageComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TechnicalComponent } from './technical/technical.component';
+import { ExtraCurricularComponent } from './extra-curricular/extra-curricular.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FunkyNavComponent } from './funky-nav/funky-nav.component';
+import { ContactComponent } from './contact/contact.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    TechnicalComponent,
+    ExtraCurricularComponent,
+    NavComponent,
+    FooterComponent,
+    FunkyNavComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
